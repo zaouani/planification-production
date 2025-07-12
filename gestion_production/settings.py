c74fa0737f3415b82e6081d8b9b3a1d630cd57e9
zaouani <hichamzaouani2002@gmail.com>
2025-07-12 14:02:44 +0100

    debg

--- gestion_production/settings.py
+++ gestion_production/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY', 'dev-key')
-DEBUG = os.environ.get('DEBUG', '') != 'False'
+DEBUG=True
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
 
 
