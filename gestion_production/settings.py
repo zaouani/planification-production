429200c192f700a22f757c1826aad5b8a2876752
zaouani <hichamzaouani2002@gmail.com>
2025-07-12 16:57:12 +0100

    jnfjhjnf

--- gestion_production/settings.py
+++ gestion_production/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY', 'dev-key')
 DEBUG = True
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
+ALLOWED_HOSTS = ['*']
 
 
 
