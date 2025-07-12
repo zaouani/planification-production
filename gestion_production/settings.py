f8b692e0fb467e828113ad73e028f901fdf87540
zaouani <hichamzaouani2002@gmail.com>
2025-07-12 13:50:21 +0100

    allawedhost

--- gestion_production/settings.py
+++ gestion_production/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY', 'dev-key')
 DEBUG = os.environ.get('DEBUG', '') != 'False'
-ALLOWED_HOSTS = ['ALLOWED_HOSTS']
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
 
 
 
