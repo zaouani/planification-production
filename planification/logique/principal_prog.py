aebc9968cca83e3d9dbd7ee3114f8c9f22cb1e76
zaouani <hichamzaouani2002@gmail.com>
2025-07-13 19:50:24 +0100

    ccc

--- planification/logique/principal_prog.py
+++ planification/logique/principal_prog.py
@@ -33,7 +33,7 @@ def generer_structure_donnees():
         
         for operateur in operateurs:
             performances = {
-                perf.tache.id_tache: perf.performance_initiale/100
+                perf.tache.id_tache: perf.performance_initiale/1000
                 for perf in operateur.performanceoperateur_set.all()
             }
             
